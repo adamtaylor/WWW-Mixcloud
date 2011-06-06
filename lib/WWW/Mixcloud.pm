947a8b706d08ae57cd33cd4ce8eb357c045915e2
Adam Taylor <adamjctaylor@gmail.com>
2011-06-07 00:17:35 +0100

    Add method to get cloudcast data

--- lib/WWW/Mixcloud.pm
+++ lib/WWW/Mixcloud.pm
@@ -1,6 +1,10 @@
 package WWW::Mixcloud;
 
 use Moose;
+use URI;
+use LWP::UserAgent;
+use JSON;
+use Data::Dump qw/ pp /;
 
 has api_key => (
     isa      => 'Str',
@@ -14,7 +18,46 @@ has api_secret => (
     required => 1,
 );
 
+has ua => (
+    is  => 'ro',
+    isa => 'LWP::UserAgent',
+    lazy_build => 1,
+);
+
+my $API_BASE = 'http://api.mixcloud.com';
+
+sub _build_ua {
+    my $self = shift;
+
+    return LWP::UserAgent->new;
+}
+
+=head2 cloudcast
+
+    my $cloudcast = $mixcloud->cloudcast( $url );
+
+=cut
+
 sub cloudcast {
+    my ( $self, $url ) = @_;
+
+    my $uri = URI->new( $url );
+
+    my $data = decode_json $self->_api_call( $uri->path );
+
+    warn pp $data;
+}
+
+sub _api_call {
+    my ( $self, $path ) = @_;
+
+    my $res = $self->ua->get( $API_BASE . $path );
+
+    if ( $res->is_success ) { 
+        #warn pp $res->content;
+    }
+
+    return $res->content;
 
 }
 
