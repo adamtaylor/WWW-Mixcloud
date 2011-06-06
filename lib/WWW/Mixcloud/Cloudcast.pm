80f8dc992a0dd7a1bceacf236e5fa72fb8b8b308
Adam Taylor <adamjctaylor@gmail.com>
2011-06-07 00:17:10 +0100

    Add cloudcast data class

--- lib/WWW/Mixcloud/Cloudcast.pm
+++ lib/WWW/Mixcloud/Cloudcast.pm
@@ -15,51 +15,88 @@ has name => (
 );
 
 has tags => (
-
+    isa      => 'ArrayRef[WWW::Mixcloud::Cloudcast::Tag]'
+    is       => 'ro',
+    required => 1,
+    default  => sub { [] },
 );
 
 has url => (
-
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
 );
 
 has pictures => (
-
+    isa      => 'ArrayRef[WWW::Mixcloud::Picture]',
+    is       => 'ro',
+    required => 1,
 );
 
 has update_time => (
-
+    isa      => 'DateTime',
+    is       => 'ro',
+    required => 1,
 );
 
 has play_count => (
-
+    isa      => 'Num',
+    is       => 'ro',
+    required => 1,
 );
 
 has comment_count => (
-
+    isa      => 'Num',
+    is       => 'ro',
+    required => 1,
 );
 
 has percentage_music => (
-
+    isa      => 'Num',
+    is       => 'ro',
+    required => 1,
 );
 
 has user => (
-
+    isa      => 'WWW::Mixcloud::User',
+    is       => 'ro',
+    required => 1,
 );
 
 has key => (
-
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
 );
 
 has created_time => (
-
+    isa      => 'DateTime',
+    is       => 'ro',
+    required => 1,
 );
 
 has audio_length => (
-
+    isa      => 'Int',
+    is       => 'ro',
+    required => 1,
 );
 
 has sections => (
+    isa      => 'ArrayRef[WWW::Mixcloud::Cloudcast::Section]',
+    is       => 'ro',
+    required => 1,
+);
 
+has slug => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
+);
+
+has description => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
 );
 
 1;
