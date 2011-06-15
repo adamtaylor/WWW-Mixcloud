225324af88a2072e2e39a38f979e2d62d857b2a3
Adam Taylor <adamjctaylor@gmail.com>
2011-06-15 22:25:27 +0100

    Finish artist data class

--- lib/WWW/Mixcloud/Artist.pm
+++ lib/WWW/Mixcloud/Artist.pm
@@ -1,7 +1,9 @@
 package WWW::Mixcloud::Artist;
 
 use Moose;
+use namespace::autoclean;
 
+use Carp qw/ croak /;
 
 has url => (
     isa      => 'Str',
@@ -26,5 +28,23 @@ has slug => (
     is       => 'ro',
     required => 1,
 );
+
+__PACKAGE__->meta->make_immutable;
+
+=head2 new_from_data
+
+=cut
+
+sub new_from_data {
+    my $class = shift;
+    my $data  = shift || croak 'Data reference required for construction';
+
+    return $class->new({
+        url  => $data->{url},
+        name => $data->{name},
+        key  => $data->{key},
+        slug => $data->{slug},
+    });
+}
 
 1;
