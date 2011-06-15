a124e3ab4b3f149b939354a3823a700e1fb2837f
Adam Taylor <adamjctaylor@gmail.com>
2011-06-15 23:01:09 +0100

    Add category data class

---
+++ lib/WWW/Mixcloud/Category.pm
@@ -0,0 +1,69 @@
+package WWW::Mixcloud::Category;
+
+use Moose;
+use namespace::autoclean;
+
+use Carp qw/ croak /;
+
+use WWW::Mixcloud::Picture;
+
+has name => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
+);
+
+has format => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
+);
+
+has url => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
+);
+
+has key => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
+);
+
+has slug => (
+    isa      => 'Str',
+    is       => 'ro',
+    required => 1,
+);
+
+has pictures => (
+    isa      => 'ArrayRef[WWW::Mixcloud::Picture]',
+    is       => 'ro',
+    required => 1,
+    default  => sub { [] },
+);
+
+__PACKAGE__->meta->make_immutable;
+
+=head2 new_from_data
+
+=cut
+
+sub new_from_data {
+    my $class = shift;
+    my $data  = shift || croak 'Data reference required for construction';
+
+    my $pictures = WWW::Mixcloud::Picture->new_list_from_data( $data->{pictures} );
+    return $class->new({
+        name     => $data->{name},
+        format   => $data->{format},
+        url      => $data->{url},
+        key      => $data->{key},
+        slug     => $data->{slug},
+        pictures => $pictures,
+    });
+
+}
+
+1;
