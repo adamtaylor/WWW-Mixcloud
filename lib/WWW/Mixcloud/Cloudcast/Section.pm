b3b8d0f0402c459e70f47d2d9f314f522fa376a1
Adam Taylor <adamjctaylor@gmail.com>
2011-06-15 22:26:15 +0100

    Finish section data class

--- lib/WWW/Mixcloud/Cloudcast/Section.pm
+++ lib/WWW/Mixcloud/Cloudcast/Section.pm
@@ -1,15 +1,14 @@
 package WWW::Mixcloud::Cloudcast::Section;
 
 use Moose;
+use namespace::autoclean;
+
+use Carp qw/ croak /;
+
+use WWW::Mixcloud::Track;
 
 has track => (
     isa      => 'WWW::Mixcloud::Track',
-    is       => 'ro',
-    required => 1,
-);
-
-has name => (
-    isa      => 'Str',
     is       => 'ro',
     required => 1,
 );
@@ -32,4 +31,29 @@ has section_type => (
     required => 1,
 );
 
+__PACKAGE__->meta->make_immutable;
+
+=head2 new_list_from_data
+
+=cut
+
+sub new_list_from_data {
+    my $class = shift;
+    my $data  = shift || croak 'Data reference required for construction';
+
+    my @sections;
+
+    foreach my $section ( @{$data} ) {
+        my $track = WWW::Mixcloud::Track->new_from_data( $section->{track} );
+
+        push @sections, $class->new({
+            track        => $track,
+            position     => $section->{position},
+            start_time   => $section->{start_time},
+            section_type => $section->{section_type},
+        })
+    }
+
+    return \@sections;
+}
 1;
